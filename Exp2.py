3c5b70cf60c2907fffaaca240aa28b39ed51c93b
AndreiMiles <124898837+AndreiMiles@users.noreply.github.com>
2023-02-10 14:22:38 +0000

    Code123

--- Exp2.py
+++ Exp2.py
@@ -1,21 +1,20 @@
 def calculate_tax(cash):
-    if cash <= 250000:
-        pay = cash 
-    elif cash > 250000 and cash <= 400000:
-        pay = (cash - 250000) * 0.15
-    elif cash > 400000 and cash <= 800000:
-        pay = 22500 + (cash - 400000) * 0.2
-    elif cash > 800000 and cash <= 200000:
-        pay = 102500 + (cash - 800000) * 0.25
-    elif cash > 2000000 and cash <= 8000000:
-        pay = 402500 + (cash - 2000000) * 0.3
-    elif cash > 8000000:
-        pay = 2205000 + (cash - 800000) * 0.35
-    else:
-        print("Not a valid input, Please try again")
-
-    return (pay)
-
+  if cash <= 250000:
+    pay = cash
+  elif cash > 250000 and cash <= 400000:
+    pay = (cash - 250000) * 0.15
+  elif cash > 400000 and cash <= 800000:
+    pay = 22500 + (cash - 400000) * 0.2
+  elif cash > 800000 and cash <= 2000000:
+    pay = 102500 + (cash - 800000) * 0.25
+  elif cash > 2000000 and cash <= 8000000:
+    pay = 402500 + (cash - 2000000) * 0.3
+  elif cash > 8000000:
+    pay = 2202500 + (cash - 8000000) * 0.35
+  else:
+    print("wrong")
+    
+  return (pay)
 
 income = float(input("Enter monthly income: "))
 annual = income * 12
@@ -23,6 +22,6 @@ taxrate = calculate_tax(annual)
 
 
 
-print("Your Yearly pay", annual) 
-print("Your monthly pay", income)
-print("Your Taxes", taxrate)
+print("Your Yearly pay: {}" .format(annual)) 
+print("Your monthly pay: {}" .format(income))
+print("Your Taxes: {}" .format(taxrate))
